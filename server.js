d7086dd5d294893710d2f3e5f94a3a7447473936
vyang14 <vyang1014888@gmail.com>
2022-08-21 02:59:58 -0400

    added fsUtils and uuid.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const fs = require('fs');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
